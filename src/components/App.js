fadad2398a440476bc1ddd6d97f5d62e496ad66a
Ali Salah Alddin <rated.ali.7@gmail.com>
2017-10-15 02:23:41 +0300

    Redirect to https

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router';
 import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+import HttpsRedirect from "react-https-redirect";
 import blue from 'material-ui/colors/blue';
 import orange from 'material-ui/colors/orange';
 import red from 'material-ui/colors/red';
@@ -18,19 +19,21 @@ const theme = createMuiTheme({
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <div>
-          <Nav>
-            <main>
-              <Switch>
-                <Route path="/browse/:mediaType/search/:search" component={MediaPage}/>
-                <Route path="/browse/:mediaType/:seasonYear/:season" component={MediaPage}/>
-                <Redirect to="/browse/anime/2017/fall"/>
-              </Switch>
-            </main>
-          </Nav>
-        </div>
-      </MuiThemeProvider>
+      <HttpsRedirect>
+        <MuiThemeProvider theme={theme}>
+          <div>
+            <Nav>
+              <main>
+                <Switch>
+                  <Route path="/browse/:mediaType/search/:search" component={MediaPage}/>
+                  <Route path="/browse/:mediaType/:seasonYear/:season" component={MediaPage}/>
+                  <Redirect to="/browse/anime/2017/fall"/>
+                </Switch>
+              </main>
+            </Nav>
+          </div>
+        </MuiThemeProvider>
+      </HttpsRedirect>
     );
   }
 }
